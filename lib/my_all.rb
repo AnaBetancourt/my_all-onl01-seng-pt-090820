5885954364f4ab4c2be1950e89606346fe67322d
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-09-16 23:52:09 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -5,9 +5,8 @@ def my_all?(collection)
   block_return_values = []
   while i < collection.length
     block_return_values << yield (collection[i])
-    i += 1 
+    i = i + 1 
   end
-  
   if block_return_values.include?(false)
     false
   else
